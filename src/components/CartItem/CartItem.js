379bc28823e54a91e7416f1334ccacfc44aa683b
Nick Callaghan <nicholas.callaghan@gmail.com>
2020-04-11 17:49:17 +0100

    Cart Styling

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -36,16 +36,18 @@ export default function CartItem(props) {
           alt="product"
         />
       </div>
-      <div className="CartItem-Info">
-        <h3>{product.name}</h3>
-        <h4>{product.tagline}</h4>
-      </div>
-      <div className="CartItem-Qty">
-        <p>{formatCurrency(product.price)}</p>
-        <div className="buttonWrapper">
-          <button onClick={(e) => handleAdjustQty(-1)}>-</button>
-          <p>{product.qty}</p>
-          <button onClick={(e) => handleAdjustQty(1)}>+</button>
+      <div className="CartItem-content">
+        <div className="CartItem-Info">
+          <h3>{product.name}</h3>
+          <h4>{product.tagline}</h4>
+        </div>
+        <div className="CartItem-Qty">
+          <p className="CartItem-price">{formatCurrency(product.price)}</p>
+          <div className="buttonWrapper">
+            <button onClick={(e) => handleAdjustQty(-1)}>-</button>
+            <p>{product.qty}</p>
+            <button onClick={(e) => handleAdjustQty(1)}>+</button>
+          </div>
         </div>
       </div>
     </div>
